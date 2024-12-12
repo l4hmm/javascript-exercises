c64de5b0b0ebe900081329d5e9cabc293822d71e
l4hmm <l4hmm@outlook.com>
2024-12-13 05:36:28 +1100

    * 2024-12-13 * [Added code] Added code to remove an element from an array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    return array.filter(element => !args.includes(element));
+
 };
 
 // Do not edit below this line
