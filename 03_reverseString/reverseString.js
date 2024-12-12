349109e2ee5aef69bf9586e849347fda383cd71d
l4hmm <l4hmm@outlook.com>
2024-12-12 19:25:34 +1100

    Fix code to return reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const splitString = str.split(""); //Splits string to return new array
+    
+    const reverseArray = splitString.reverse(); //Flips split array
+
+    const joinArray = reverseArray.join(""); // Re joins flipped array
+
+    return joinArray; // Joins split array together. 
 
 };
 
